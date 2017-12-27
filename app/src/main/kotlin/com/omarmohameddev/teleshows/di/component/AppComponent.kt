9307d6e9beb825dd33e0b757eff299201e0afc51
OmarMohamedDev <omar.mohamed.dev@gmail.com>
2017-12-27 13:04:21 +0100

    Added builder inside AppComponent

--- app/src/main/kotlin/com/omarmohameddev/teleshows/di/component/AppComponent.kt
+++ app/src/main/kotlin/com/omarmohameddev/teleshows/di/component/AppComponent.kt
@@ -1,18 +1,29 @@
 package com.omarmohameddev.teleshows.di.component
 
+import android.app.Application
 import com.omarmohameddev.teleshows.TeleshowsApp
 import com.omarmohameddev.teleshows.di.module.AppModule
 import com.omarmohameddev.teleshows.di.module.DetailModule
 import com.omarmohameddev.teleshows.di.module.ListModule
+import com.omarmohameddev.teleshows.di.scope.ApplicationScope
+import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
+@ApplicationScope
 @Singleton
 @Component(modules = arrayOf(
         AppModule::class
 ))
-
 interface AppComponent {
+
+    //Builder
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+        fun build(): AppComponent
+    }
 
     // Injectors
     fun injectTo(app: TeleshowsApp)
