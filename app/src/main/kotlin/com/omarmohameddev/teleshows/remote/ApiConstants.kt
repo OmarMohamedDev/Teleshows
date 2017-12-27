661bf1db2f7e911119094cb50205d4b1cde6f875
OmarMohamedDev <omar.mohamed.dev@gmail.com>
2017-12-27 08:49:10 +0100

    Added okhttp and retrofit dependecies and created Service classes to handle network connection

---
+++ app/src/main/kotlin/com/omarmohameddev/teleshows/remote/ApiConstants.kt
@@ -0,0 +1,7 @@
+package com.omarmohameddev.teleshows.remote
+
+object ApiConstants {
+    const val BASE_URL = "https://api.themoviedb.org/3"
+    const val CONNECTION_TIMEOUT: Long = 120
+    const val DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+}
---
+++ app/src/main/kotlin/com/omarmohameddev/teleshows/remote/TeleshowsServiceFactory.kt
@@ -0,0 +1,60 @@
+package com.omarmohameddev.teleshows.remote
+
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+/**
+ * Provide "make" methods to create instances of [TeleshowsService]
+ * and its related dependencies, such as OkHttpClient, Gson, etc.
+ */
+object TeleshowsServiceFactory {
+
+    fun makeTeleshowsService(isDebug: Boolean): TeleshowsService {
+        val okHttpClient = makeOkHttpClient(
+                makeLoggingInterceptor(isDebug))
+        return makeTeleshowsService(okHttpClient, makeGson())
+    }
+
+    private fun makeTeleshowsService(okHttpClient: OkHttpClient, gson: Gson): TeleshowsService {
+        val retrofit = Retrofit.Builder()
+                .baseUrl(ApiConstants.BASE_URL)
+                .client(okHttpClient)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+        return retrofit.create(TeleshowsService::class.java)
+    }
+
+    private fun makeOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+                .addInterceptor(httpLoggingInterceptor)
+                .connectTimeout(ApiConstants.CONNECTION_TIMEOUT, TimeUnit.SECONDS)
+                .readTimeout(ApiConstants.CONNECTION_TIMEOUT, TimeUnit.SECONDS)
+                .build()
+    }
+
+    private fun makeGson(): Gson {
+        return GsonBuilder()
+                .setLenient()
+                .setDateFormat(ApiConstants.DATE_FORMAT)
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create()
+    }
+
+    private fun makeLoggingInterceptor(isDebug: Boolean): HttpLoggingInterceptor {
+        val logging = HttpLoggingInterceptor()
+        logging.level = if (isDebug)
+            HttpLoggingInterceptor.Level.BODY
+        else
+            HttpLoggingInterceptor.Level.NONE
+        return logging
+    }
+
+}
